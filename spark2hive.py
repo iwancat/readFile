345094ba59a0ff6cb128790459fdc48d0a4e2507
yuehaoan <yuehaoan@163.com>
2022-06-09 11:17:31 +0800

    some change

--- spark2hive.py
+++ spark2hive.py
@@ -27,7 +27,7 @@ def col_spark():
     sc = SparkContext(conf=conf)
     hc = HiveContext(sc)
     hc.setConf('hive.exec.dynamic.partition.mode', 'nonstrict')
-    result = hc.sql("create table duizhang.stat_aaa_2022030901 as select count(1) from duizhang.stat_hc_userlist_4")
+    result = hc.sql("create table stat_aaa_2022030901 as select count(1) from userlist_4")
     print("===========")
     print(result)
     print("===========")
@@ -39,28 +39,28 @@ def write_to_hdfs(client, hdfs_path, data):
 
 
 def col_spark2():
-    conf2 = SparkConf().setAppName("IOTAA" + "GPRS" + "2")
+    conf2 = SparkConf().setAppName("AA" + "bb" + "2")
     sc2 = SparkContext(conf=conf2)
     hc2 = HiveContext(sc2)
     hc2.setConf('hive.exec.dynamic.partition.mode', 'nonstrict')
-    #    sqlong = "create table duizhang.stat_ccc_sp_20220310 as select * from 表 where arrive_time>=20210927170000 and arrive_time<20210927180000"
-    sqlong = "select trademark,bill_month,hplmn1,count(1) ddct from duizhang.stat_ccc_sp_20220310 group by trademark,bill_month,hplmn1"
+    #    sqlong = "create table stat_ccc_sp_20220310 as select * from 表 where arrive_time>=20210927170000 and arrive_time<20210927180000"
+    sqlong = "select mmonth,count(1) ddct from stat_ccc_20220310 group by  mmonth"
     result2 = hc2.sql(sqlong)
     sslist =  result2.toPandas()
 
-    #filepa = "/home/ochadoop/asiainfo/anhy/spark/slisresult.csv"  #屏蔽写本地
+    #filepa = "/home/hadoop/anhy/spark/slisresult.csv"  #屏蔽写本地
     # ofnt = open(filepa, "w+")       #屏蔽写本地
     hdfile = []
     #ofnt = open(filepa, "w+")        #屏蔽写本地
 
     for slist in sslist.itertuples():
-        linetxt= str(getattr(slist, 'trademark'))+"=====" + str(getattr(slist, 'bill_month')) + "G000000G" + str(getattr(slist, 'hplmn1')) + str(getattr(slist, 'ddct'))+"\n"
+        linetxt= str(getattr(slist, 'ddd'))+"=====" + str(getattr(slist, 'vvv')) + "G000000G" + str(getattr(slist, 'hhh')) + str(getattr(slist, 'ddct'))+"\n"
         #   ofnt.writelines(linetxt)  #屏蔽写本地
         hdfile.append(linetxt)
     #ofnt.close()                     #屏蔽写本地
 
     print("next is hdfs write")
-    hd_path = "/duizhang/qwmonitor/100-001-002.csv"
+    hd_path = "/qwmonitor/100-001-002.csv"
     client = Client("http://你的主机ip:50070/", root="/", timeout=10000, session=False)
     client.write(hd_path, hdfile, overwrite=True, append=False, encoding='utf-8')
     print("hdfs is write ok")
@@ -111,7 +111,7 @@ PYSPARK_PYTHON=/home/hadoop/apps/Python3/bin/python3 spark-submit \
 --driver-memory 12G \
 --archives hdfs:///duizhang/Python3.zip#ANACONDA \
 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/home/hadoop/apps/Python3/bin/python3 \
---files /home/ochadoop/apps/spark/conf/hive-site.xml \
+--files /home/hadoop/apps/spark/conf/hive-site.xml \
 spark2hive_sprd.py
 '''
 
